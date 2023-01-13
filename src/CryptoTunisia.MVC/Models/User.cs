eabee971847d0b6537464ac89255f389490213fa
Omar <omar.jabloun@insat.u-carthage.tn>
2023-01-13 23:29:02 +0100

    update User

--- src/CryptoTunisia.MVC/Models/User.cs
+++ src/CryptoTunisia.MVC/Models/User.cs
@@ -2,7 +2,7 @@ namespace CryptoTunisia.MVC.Models;
 
 public class User
 {
-    public string Username { get; set; }
+    public string UserName { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
     public string Role { get; set; }
